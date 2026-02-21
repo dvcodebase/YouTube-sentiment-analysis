4bcf313ac4ad277564396dbb1505557d6919c3ed
Dheerajverma2004 <dheerajverma.cp@gmail.com>
2026-02-22 00:27:57 +0530

    preprocessing-EDA and trained models

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'YouTube_Comment_Sentiment_Analyser',
+    version= '0.0.0',
+    author= 'Dheeraj verma',
+    author_email= 'dheerajverma.cp@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
