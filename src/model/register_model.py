1e903f5f819d46ed5dc799f1b48ebd2ee88d2c67
Dheerajverma2004 <dheerajverma.cp@gmail.com>
2026-02-22 15:41:51 +0530

    release: v1 local setup with Anaconda environment support

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-13-204-252-144.ap-south-1.compute.amazonaws.com:5000")
+mlflow.set_tracking_uri("http://ec2-13-204-252-144.ap-south-1.compute.amazonaws.com:5000/")
 
 
 # logging configuration
